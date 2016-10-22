3f4a157b9915c74d35b1c02feca928dc830287ce
Will Graham <grahamw0@students.rowan.edu>
2016-10-22 13:14:34 -0400

    Compute() seems to work!

--- src/postfix/InfixCalc.java
+++ src/postfix/InfixCalc.java
@@ -20,6 +20,7 @@ public class InfixCalc {
     this.expression = expression;
     this.postExpression = "";
     validate();
+    compute();
   }
 
   private void validate() {
@@ -36,8 +37,70 @@ public class InfixCalc {
           throw new ArithmeticException("Parenthesis Mismatch.");
       }
     }
-    if(!stack.isEmpty())
+    if (!stack.isEmpty())
       throw new ArithmeticException("Parenthesis Mismatch.");
+  }
+
+  private void compute() {
+    Stack opStack = new Stack();
+    String splitOperWhiteSpace = "(((?<=\\()|(?=\\())|((?<=\\))|(?=\\)))|\\s+)";
+    String[] exprArray = expression.trim().split(splitOperWhiteSpace);
+    for (String token : exprArray) {
+      if (isNumber(token)) { // Number
+        postExpression += token + " ";
+      } else if (token.equals(")")) {
+        while (!opStack.isEmpty() && !opStack.peek().equals("(")) {
+          postExpression += opStack.pop() + " ";
+        }
+      } else if (token.equals("(")) {
+        opStack.push(token);
+      } else if (isOperator(token)) {
+        if (opStack.isEmpty()) {
+          opStack.push(token);
+        } else if (!isOperator(opStack.peek())) {
+          opStack.push(token);
+        } else if (getPrecedence(token) > getPrecedence(opStack.peek())) {
+          opStack.push(token);
+        } else {
+          while(opStack.peek() != null && getPrecedence(token) <= getPrecedence(opStack.peek())) {
+            postExpression += opStack.pop() + " ";
+          }
+          opStack.push(token);
+        }
+      }
+    }
+    while (!opStack.isEmpty()) {
+      if(opStack.peek().equals("(")) {
+        opStack.pop();
+      }
+      else {
+        postExpression += opStack.pop() + " ";
+      }
+    }
+    new PostCalc(postExpression);
+    //System.out.println(postExpression); // TODO: Remove test statement
+  }
+
+
+  private boolean isNumber(String s) {
+    return s.matches("^-?\\d+$");
+  }
+
+  private boolean isOperator(String s) {
+    return s.matches("^[+-/*//]$");
+  }
+
+  private int getPrecedence(String op) {
+    switch (op) {
+      case "+":
+      case "-":
+        return 1;
+      case "*":
+      case "/":
+        return 2;
+      default:
+        return -1; // Error; should be unreachable
+    }
   }
 
 }
