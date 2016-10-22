a7e0ead6212ea1b8e8bc2b4d69b878a27a93249d
Will <grahamw0@students.rowan.edu>
2016-10-22 01:33:31 -0400

    Testing

--- src/postfix/Driver.java
+++ src/postfix/Driver.java
@@ -13,8 +13,8 @@ public class Driver {
    * @param args
    */
   public static void main(String[] args) {
-    //PostCalc test = new PostCalc("3 2 *");
-    InfixCalc test = new InfixCalc("(((2 + 3))");
+    PostCalc test = new PostCalc("3 2 + -");
+    //InfixCalc test = new InfixCalc("(((2 + 3))");
     
   }
 
