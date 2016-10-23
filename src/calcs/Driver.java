23f374da3ef0d6c434d381b4168843f3e66eb98f
ryan <ryan@10.0.0.36>
2016-10-23 16:55:59 -0400

    Started the doc on Class PostCalc

--- src/calcs/Driver.java
+++ src/calcs/Driver.java
@@ -13,8 +13,8 @@ public class Driver {
    * @param args
    */
   public static void main(String[] args) {
-    //PostCalc test = new PostCalc("1 2 3 * 5 + +");
-    InfixCalc test = new InfixCalc("2 * 3 * 1 + 2");
+    PostCalc test = new PostCalc("1 2 3 * 5 + +");
+    //InfixCalc test = new InfixCalc("2 * 3 * 1 + 2");
     
     
     /*String testString = "(31 + 234)";
--- src/calcs/PostCalc.java
+++ src/calcs/PostCalc.java
@@ -4,28 +4,44 @@
 package calcs;
 
 /**
- * @author grahamw0
+ * This is Class PostCalc.  It houses the logic to 
+ * take in a postfix expression and compute that 
+ * expression to output an int result.  
+ * @author Will Graham
  *
  */
 public class PostCalc {
-  private Stack stack;
-  private String expression;
-
+  private Stack stack; 
+  private String expression; // The input String
+  
+  /**
+   * This is the constructor for class PostCalc.
+   * When a PostCalc object is created it will take
+   * the input parameter and initialize it to the 
+   * expression field.  It will also call the validate() 
+   * and compute() methods.  
+   * @param expression The input string. 
+   */
   public PostCalc(String expression) {
     this.expression = expression;
     validate();
     compute();
   }
-
+  
+  /**
+   * The validate() method will parse through
+   * the input string one char at a time.  It
+   * will check to see if each character is valid.
+   */
   private void validate() {
-    String[] array = expression.trim().split("\\s+");
+    String[] array = expression.trim().split("\\s+"); // Take the input string and split it up into an array.  
     int counter = 0;
     boolean wentNeg = false;
 
-    for (int i = 0; i < array.length; i++) {
-      if (array[i].matches("^-?\\d+$")) {
+    for (int i = 0; i < array.length; i++) { // Iterate through the array.
+      if (array[i].matches("^-?\\d+$")) { // regex to check if it is a number.  
         counter++;
-      } else if (array[i].matches("^[+-/*//]$")) {
+      } else if (array[i].matches("^[+-/*//]$")) { // Regex to check if it is an operator.
         counter--;
         if (counter < 0)
           wentNeg = true;
@@ -40,18 +56,24 @@ public class PostCalc {
       throw new ArithmeticException("Invalid Postfix syntax.");
 
   }
-
+  
+  /**
+   * The compute() method will be called from the constructor.
+   * It will take in the input String and split it up into
+   * an array.  It will then iterate through each char and 
+   * compute the given postfix expression.  
+   */
   public void compute() {
-    String[] array = expression.trim().split("\\s+");
+    String[] array = expression.trim().split("\\s+"); // Take the input string and split it up into an array.
     Stack stack = new Stack();
     
-    for(int i = 0; i < array.length; i++) {
-      if(array[i].matches("^-?\\d+$")) {  // Number
-        stack.push(array[i]);
+    for(int i = 0; i < array.length; i++) { // Iterate through the array.
+      if(array[i].matches("^-?\\d+$")) {  // If the char is a number then push it.  Regex for a number. 
+        stack.push(array[i]); 
       }
-      else {  // Operator
+      else {  // Use a switch to handle the individual case depending on the operator. 
         int result;
-        int first = Integer.valueOf(stack.pop());
+        int first = Integer.valueOf(stack.pop()); // Create an int variable equal to the value of the char. 
         int second = Integer.valueOf(stack.pop());
         switch (array[i]) {
           case "+":
@@ -76,10 +98,6 @@ public class PostCalc {
         }
       }
     }
-    System.out.println(stack.pop());
-    
-    
+    System.out.println(stack.pop()); // pop the result
   }
-
-
 }
