af7ded2b2d30a6eac3f4dd12817a7523b3de912b
Will <grahamw0@students.rowan.edu>
2016-10-25 00:16:11 -0400

    Knight's Tour potentially working
    If it does work, it works slowly as the board gets larger and starting
    positions get more spread out from 0,0

--- src/knightsTour/Driver.java
+++ src/knightsTour/Driver.java
@@ -13,8 +13,11 @@ public class Driver {
    * @param args
    */
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-
+    long startTime = System.currentTimeMillis();
+    Tour test = new Tour(9, 0, 0);
+    long stopTime = System.currentTimeMillis();
+    double elapsedTime = (stopTime - startTime) * 0.001;
+    System.out.println(elapsedTime + " seconds");
   }
 
 }
