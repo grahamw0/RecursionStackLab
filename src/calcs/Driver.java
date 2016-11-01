0c334c83f357d2e914a33e6a9246cd225a70107e
Will Graham <grahamw0@students.rowan.edu>
2016-11-01 11:27:55 -0400

    Comment and format updated.

--- src/calcs/Driver.java
+++ src/calcs/Driver.java
@@ -1,17 +1,13 @@
 /**
- * 
+ * Driver for both types of calculators, with examples of usage.
  */
 package calcs;
 
 /**
- * @author grahamw0
+ * @author Ryan Godfrey, Will Graham
  *
  */
 public class Driver {
-
-  /**
-   * @param args
-   */
   public static void main(String[] args) {
     new InfixCalc("((2 * 3)) + (3 / 2)"); // Example call
     new PostCalc("2 3 * 3 2 / +"); // Example call
