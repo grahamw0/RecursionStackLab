1b504e99ef74033769169b108e6b6a905f24d0b3
Will Graham <grahamw0@students.rowan.edu>
2016-11-01 12:44:49 -0400

    Commented.

--- src/knightsTour/Tour.java
+++ src/knightsTour/Tour.java
@@ -1,48 +1,36 @@
-/**
- * 
- */
 package knightsTour;
 
 /**
- * @author grahamw0
+ * Actually runs the Knight's Tour, and handles the logic of movement and potentially failed Tours.
+ * 
+ * @author Ryan Godfrey, Will Graham
  *
  */
 public class Tour {
+  /**
+   * Constructor and logic driver.
+   * 
+   * @param boardSize Size of each side of the square board
+   * @param xStart The x-coordinate the Knight will start on
+   * @param yStart The y-coordinate the Knight will start on
+   */
   public Tour(int boardSize, int xStart, int yStart) {
-    if (boardSize <= 4)
+    if (boardSize <= 4) // No Tours are possible on a board less than size 5
       System.out.println("No Knight's Tour possible.");
     else {
-
       Board board = new Board(boardSize);
       Coord startLocation = new Coord(xStart, yStart);
       Knight knight = new Knight(startLocation, board);
 
-      while (knight.canMove())
+      while (knight.canMove()) // As long as it can, move the Knight
         knight.move();
 
+      // If the Knight can't move, but the number of steps it took isn't the # of board tiles
       if (board.getSteps() != boardSize * boardSize) {
-        System.out.println("RECURSING"); //TODO: Remove test statement
-        new Tour(boardSize, xStart, yStart);
+        new Tour(boardSize, xStart, yStart); // Recursively find another path
       } else {
         System.out.println(board);
       }
-    }
-  }
-
-  public static boolean run(int boardSize, int xStart, int yStart) {
-    Board board = new Board(boardSize);
-    Coord startLocation = new Coord(xStart, yStart);
-    Knight knight = new Knight(startLocation, board);
-
-    while (knight.canMove())
-      knight.move();
-
-    if (board.getSteps() != boardSize * boardSize) {
-      // System.out.println("No Knight's Tour possible.");
-      return false;
-    } else {
-      // System.out.println(board);
-      return true;
     }
   }
 
