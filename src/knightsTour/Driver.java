e890fd7c35aa9e6f441fc0544ee34df21e470d9c
Will Graham <grahamw0@students.rowan.edu>
2016-11-01 12:49:52 -0400

    Commented and formatted.

--- src/knightsTour/Driver.java
+++ src/knightsTour/Driver.java
@@ -1,39 +1,13 @@
-/**
- * 
- */
 package knightsTour;
 
-import java.util.ArrayList;
-
 /**
- * @author grahamw0
+ * Driver for the Knight's Tour, with a usage example.
+ * 
+ * @author Ryan Godfrey, Will Graham
  *
  */
 public class Driver {
-
-  /**
-   * @param args
-   */
   public static void main(String[] args) {
-    /*long startTime = System.currentTimeMillis();
-    Tour test = new Tour(10, 0, 0);
-    long stopTime = System.currentTimeMillis();
-    double elapsedTime = (stopTime - startTime) * 0.001;
-    System.out.println(elapsedTime + " seconds");*/
-    
-    new TourV2(20, 5, 7);
-    /*ArrayList<Boolean> list = new ArrayList<>();
-    for(int i = 0; i < 500000; i++) {
-      list.add(TourV2.run(10, 0, 0));
-    }
-    
-    int falses = 0;
-    for(Boolean b : list) {
-      if (!b)
-        falses++;
-    }
-    System.out.println(falses);*/
-    
+    new Tour(10, 5, 4); // Example call
   }
-
 }
